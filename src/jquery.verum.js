5278c26d13ab2be2f797aaadaf906136ee2ab44a
Jack Scott <123.jack.scott@gmail.com>
2017-08-12 14:24:01 +1200

    Documented code

--- src/jquery.verum.js
+++ src/jquery.verum.js
@@ -1,14 +1,46 @@
+/**
+MIT License
+
+Copyright (c) 2017 Jack Scott
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+ */
 $.fn.verum = function (options) {
+    // Default settings
     var defaults = {
         'passClass': 'validation-pass',
         'failClass': 'validation-pass',
         'feedbackClass': 'validation-feedback'
     };
+    // Merge default settings with options passed in.
     var settings = $.extend(defaults, options);
     var rules = settings.rules;
     var excluded = settings.excluded;
     var form = this;
+
+    /**
+     * Form submit listener.
+     * When the form is submitted, run validation and only submit if all passes.
+     */
     form.submit(function (e) {
+        // Loop through each item in the form and validate it.
         form.children().each(function () {
             var formObject = $(this).find('input, select, textarea');
             if (!excluded.contains(formObject.attr('name'))) {
@@ -21,9 +53,12 @@ $.fn.verum = function (options) {
             return true;
         });
     });
+
+    // Loop through each item in the form and add a keyup listener.
     this.children().each(function () {
         var formObject = $(this).find('input, select, textarea');
         if (!excluded.contains(formObject.attr('name'))) {
+            // On each keyup, validate the item.
             formObject.keyup(function () {
                 validate(rules, formObject, settings);
 
@@ -32,6 +67,15 @@ $.fn.verum = function (options) {
     });
 
 };
+
+/**
+ * Validation function.
+ *
+ * @param rules A list of rules separated by |
+ * @param formObject The object to validate.
+ * @param settings The array of settings.
+ * @returns {boolean} True if pass, false if fail.
+ */
 function validate(rules, formObject, settings) {
     var objectName = formObject.attr('name');
     var ret = true;
@@ -69,6 +113,14 @@ function validate(rules, formObject, settings) {
     }
     return ret;
 }
+
+/**
+ * Checks a given input by a given rule.
+ * @param input The string to test.
+ * @param rule The rule to check.
+ * @param extra Extra data needed for the rule such as min/max length
+ * @returns {*}
+ */
 function checkRule(input, rule, extra) {
     switch (rule) {
         case 'min':
